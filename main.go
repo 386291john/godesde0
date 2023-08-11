d607ff0c6809fa5bd0913b285eef04383c13c250
386291john <srcaballero144@gmail.com>
2023-08-10 23:46:13 -0500

    commit con modificacion de tipo variables

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/386291john/godesde0/variables"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	variables.MuestroEnteros()
+	/*estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)*/
+
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es Windows, es ", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
