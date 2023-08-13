1a05b22156548181c59d6b44a19b597e7652fb81
386291john <srcaballero144@gmail.com>
2023-08-12 23:19:43 -0500

    commit datos ingresados por pantalla

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"github.com/386291john/godesde0/ejercicios"
-)
+import "github.com/386291john/godesde0/teclado"
 
 func main() {
 	/*estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
 	fmt.Println(texto)*/
 
-	num, texto := ejercicios.Ejercicio("dddd")
+	/*num, texto := ejercicios.Ejercicio("dddd")
 	fmt.Println(num)
-	fmt.Println(texto)
+	fmt.Println(texto)*/
 
 	/*if os := runtime.GOOS; os == "linux" || os == "OS X." {
 		fmt.Println("Esto no es Windows, es ", os)
@@ -28,4 +25,5 @@ func main() {
 	default:
 		fmt.Printf("%s \n", os)
 	}*/
+	teclado.IngresoNumeros()
 }
